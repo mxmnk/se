debbab640b2cf43450716bab6b0c2978eefed4fc
George Maximenko <30684663+mvximenko@users.noreply.github.com>
2020-06-25 22:13:00 +0900

    Add files via upload

---
+++ bigO.js
@@ -0,0 +1,16 @@
+// Big O of Object Methods
+// Object.keys - O(N)
+// Object.values - O(N)
+// Object.entries - O(N)
+// hasOwnProperty - O(1)
+
+// Big O of Array Operations
+// push - O(1)
+// pop - O(1)
+// shift - O(N)
+// unshift - O(N)
+// concat - O(N)
+// slice - O(N)
+// splice - O(N)
+// sort - O(N * log N)
+// forEach/map/filter/reduce/etc. - O(N)
