d8ea4ce04b5a657c92961bb3174cce5e70368e4a
George Maximenko <30684663+mvximenko@users.noreply.github.com>
2020-06-30 19:41:36 +0900

    Add files via upload

---
+++ Sorting Algorithms/Selection Sort.js
@@ -0,0 +1,34 @@
+function selectionSort(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    let lowest = i;
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[j] < arr[lowest]) {
+        lowest = j;
+      }
+    }
+    if (i !== lowest) {
+      let temp = arr[i];
+      arr[i] = arr[lowest];
+      arr[lowest] = temp;
+    }
+  }
+  return arr;
+}
+
+function selectionSort(arr) {
+  const swap = (arr, idx1, idx2) =>
+    ([arr[idx1], arr[idx2]] = [arr[idx2], arr[idx1]]);
+
+  for (let i = 0; i < arr.length; i++) {
+    let lowest = i;
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[lowest] > arr[j]) {
+        lowest = j;
+      }
+    }
+    if (i !== lowest) swap(arr, i, lowest);
+  }
+  return arr;
+}
+
+selectionSort([0, 2, 34, 22, 10, 19, 17]);
