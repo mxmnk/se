c8cc176412fe6b7b0724c6cde6ee984a2650425b
George Maximenko <30684663+mvximenko@users.noreply.github.com>
2020-06-30 20:43:15 +0900

    Add files via upload

---
+++ Sorting Algorithms/Merge Sort.js
@@ -0,0 +1,33 @@
+function merge(arr1, arr2) {
+    let results = [];
+    let i = 0;
+    let j = 0;
+    while (i < arr1.length && j < arr2.length) {
+        if (arr2[j] > arr1[i]) {
+            results.push(arr1[i]);
+            i++;
+        } else {
+            results.push(arr2[j]);
+            j++;
+        }
+    }
+    while (i < arr1.length) {
+        results.push(arr1[i]);
+        i++;
+    }
+    while (j < arr2.length) {
+        results.push(arr2[j]);
+        j++;
+    }
+    return results;
+}
+
+function mergeSort(arr) {
+    if (arr.length <= 1) return arr;
+    let mid = Math.floor(arr.length / 2);
+    let left = mergeSort(arr.slice(0, mid));
+    let right = mergeSort(arr.slice(mid));
+    return merge(left, sright);
+}
+
+mergeSort([10, 24, 76, 73]);
